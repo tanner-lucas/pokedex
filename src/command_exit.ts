d4a43e2c3c643603e963bb1a3836de2ef17421e0
tanner <tannerlucasdev@gmail.com>
2026-08-31 14:53:34 -0500

    close cache before exit to avoid hang

--- src/command_exit.ts
+++ src/command_exit.ts
@@ -3,5 +3,6 @@ import type { State } from "./state.js";
 export async function commandExit(state: State) {
     console.log("Closing the Pokedex... Goodbye!");
     state.rl.close();
+    state.pokeapi.closeCache();
     process.exit(0);
 }
