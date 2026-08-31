6b8f6300caf18d5c25af6b2f0734165d54520c66
tanner <tannerlucasdev@gmail.com>
2026-08-31 15:17:41 -0500

    add page number helper function

--- src/command_map.ts
+++ src/command_map.ts
@@ -2,6 +2,8 @@ import type { State } from "./state.js";
 
 export async function commandMap(state: State) {
 
+    const page = pageFromURL(state.nextLocationsURL);
+
     const locations = await state.pokeapi.fetchLocations(
         state.nextLocationsURL ?? undefined,
     );
@@ -9,8 +11,17 @@ export async function commandMap(state: State) {
     state.nextLocationsURL = locations.next;
     state.prevLocationsURL = locations.previous;
 
+    console.log(`page: ${page}`);
     for (const loc of locations.results) {
         console.log(loc.name);
     }
 
+}
+
+export function pageFromURL(url: string | null): number {
+    if (!url) {
+        return 1;
+    }
+    const offset = Number(new URL(url).searchParams.get("offset") ?? "0");
+    return offset / 20 + 1;
 }
--- src/command_mapb.ts
+++ src/command_mapb.ts
@@ -1,6 +1,9 @@
 import type { State } from "./state.js";
+import { pageFromURL } from "./command_map.js";
 
 export async function commandMapB(state: State) {
+
+    const page = pageFromURL(state.prevLocationsURL);
 
     if (!state.prevLocationsURL) {
         console.log("you're on the first page");
@@ -13,6 +16,8 @@ export async function commandMapB(state: State) {
 
     state.nextLocationsURL = locations.next;
     state.prevLocationsURL = locations.previous;
+
+    console.log(`page: ${page}`)
 
     for (const loc of locations.results) {
         console.log(loc.name);
